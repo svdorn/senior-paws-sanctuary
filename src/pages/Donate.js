d9aef2b1b4432156c9bcb5c94114b3112332e70a
Stephen Dorn <steved11@sbcglobal.net>
2019-04-23 21:08:02 -0700

    get started on donate button

--- src/pages/Donate.js
+++ src/pages/Donate.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
 import Swirl from "../components/Swirl";
 
 import "./Pages.css";
@@ -10,10 +11,24 @@ class Donate extends React.Component {
                 <div className="first-frame-container home-first-frame">
                     <Swirl fill="#edf7fc" />
                     Donate
+                    <DonateButton />
                 </div>
             </div>
         );
     }
 }
+
+const DonateButton = () => (
+    <div className="button-container">
+        <Button
+            target="_blank"
+            href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=Z989F9DMQU74N&currency_code=USD&source=url"
+            className="donate-button"
+            size="large"
+        >
+            Donate
+        </Button>
+    </div>
+);
 
 export default Donate;
