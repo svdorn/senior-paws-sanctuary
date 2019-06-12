7f2a3d3d7ed29c689bd17b97b098871d819b998d
Stephen Dorn <steved11@sbcglobal.net>
2019-06-12 12:45:37 -0700

    donate and subscription on donation page

--- src/pages/Donate.js
+++ src/pages/Donate.js
@@ -8,11 +8,29 @@ class Donate extends React.Component {
     render() {
         return (
             <div className="container">
-                <div className="first-frame-container home-first-frame">
+                <div className="first-frame-container first-frame-header background-yellow">
                     <Swirl fill="#edf7fc" />
-                    Donate
-                    <DonateButton />
-                    <SubscribeButton />
+                    <h1 className="header">Donate</h1>
+                </div>
+                <div className="donate">
+                    <div>
+                        <h3>One-Time Donation</h3>
+                        <img src={"/images/Icons/dog3.svg"} alt="Dog" height={60} />
+                        <div>
+                            Make a one-time donation to help give senior dogs a comfortable home to
+                            live out their days in peace.
+                        </div>
+                        <DonateButton />
+                    </div>
+                    <div>
+                        <h3>Monthly Subscription</h3>
+                        <img src={"/images/Icons/dog1.svg"} alt="Dog" height={60} />
+                        <div>
+                            Contribute a set amount each month to provide a comfortable, loving and
+                            safe environment for senior dogs.
+                        </div>
+                        <SubscribeButton />
+                    </div>
                 </div>
             </div>
         );
